84b25bf4814b8f95263f927b8352c3d25e6eacaf
edevars <e.devars.dev@gmail.com>
2019-07-07 16:57:33 -0500

    Adding user from jsonplaceholder

--- src/App.js
+++ src/App.js
@@ -1,9 +1,48 @@
-import React from 'react';
+import React, {Component} from "react";
 
-function App() {
-  return (
-    <h1>Hola mundo!</h1>
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      usuarios: []
+    };
+  }
+
+  componentDidMount() {
+    this.fetchUsers();
+  }
+
+  fetchUsers = async () => {
+    const response = await fetch(`https://jsonplaceholder.typicode.com/users`);
+    let data = await response.json();
+    this.setState({ usuarios: data });
+  };
+
+  ponerFilas = () =>
+    this.state.usuarios.map(usuario => (
+      <tr key={usuario.id}>
+        <td>{usuario.name}</td>
+        <td>{usuario.email}</td>
+        <td>{usuario.website}</td>
+      </tr>
+    ));
+
+  render() {
+    return (
+      <div className="margen">
+        <table className="tabla">
+          <thead>
+            <tr>
+              <th>Nombre</th>
+              <th>Correo</th>
+              <th>Enlace</th>
+            </tr>
+          </thead>
+          <tbody>{this.ponerFilas()}</tbody>
+        </table>
+      </div>
     );
+  }
 }
 
 export default App;
