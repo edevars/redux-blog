2828b26c21aa34c2ddbb6f018096001a908ede1f
edevars <e.devars.dev@gmail.com>
2019-09-23 23:03:47 -0500

    :bug: Fixing Layout typo

--- src/Components/Layout.js
+++ src/Components/Layout.js
@@ -5,7 +5,7 @@ const Layout = (props) => (
     <>
         <nav className="navbar is-dark" role="navigation" aria-label="main navigation">
             <div className="navbar-brand">
-                <a class="navbar-item" href="https://bulma.io">
+                <a className="navbar-item" href="https://bulma.io">
                     <img alt="Logo" src="https://lh3.googleusercontent.com/Ax2wQYxjDITuZEpc6K9EDYPG7C839tb4PApia4Tmf18u8XehB-twqhVgDVPgxxExkr4" />
                 </a>
                 <Link className="navbar-link is-primary" to="/">Usuarios</Link>
