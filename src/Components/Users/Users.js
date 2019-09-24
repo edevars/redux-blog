1d25d4027e4004220a1afb3ef69f846170467d22
edevars <e.devars.dev@gmail.com>
2019-09-23 23:03:27 -0500

    :sparkles: Connecting the users page with redux and reducers

--- src/Components/Users/Users.js
+++ src/Components/Users/Users.js
@@ -1,16 +1,12 @@
 import React, { Component } from "react";
+import { connect } from "react-redux";
 
 class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      usuarios: []
-    };
-  }
+ 
 
-  componentDidMount() {
-    this.fetchUsers();
-  }
+  // componentDidMount() {
+  //   this.fetchUsers();
+  // }
 
   fetchUsers = async () => {
     const response = await fetch(`https://jsonplaceholder.typicode.com/users`);
@@ -18,19 +14,14 @@ class App extends Component {
     this.setState({ usuarios: data });
   };
 
-  renderUsers = () => this.state.usuarios.map((usuario) => (
-    <tr key={usuario.id}>
-      <td>
-        {usuario.name}
-      </td>
-      <td>
-        {usuario.email.toLowerCase()}
-      </td>
-      <td>
-        {usuario.website}
-      </td>
-    </tr>
-  ));
+  renderUsers = () =>
+    this.props.usuarios.map(usuario => (
+      <tr key={usuario.id}>
+        <td>{usuario.name}</td>
+        <td>{usuario.email.toLowerCase()}</td>
+        <td>{usuario.website}</td>
+      </tr>
+    ));
 
   render() {
     return (
@@ -39,18 +30,31 @@ class App extends Component {
         <table className="table is-hoverable is-fullwidth">
           <thead>
             <tr className="is-selected">
-              <th><strong title="Usuario">Usuario</strong></th>
-              <th><strong title="E-mail">E-mail</strong></th>
-              <th><strong title="Website">Website</strong></th>
+              <th>
+                <strong title="Usuario">Usuario</strong>
+              </th>
+              <th>
+                <strong title="E-mail">E-mail</strong>
+              </th>
+              <th>
+                <strong title="Website">Website</strong>
+              </th>
             </tr>
           </thead>
-          <tbody>
-            {this.renderUsers()}
-          </tbody>
+          <tbody>{this.renderUsers()}</tbody>
         </table>
       </div>
-    )
+    );
   }
 }
 
-export default App;
+const mapStateToProps = reducers => {
+  return reducers.usuariosReducer;
+};
+
+export default connect(
+  mapStateToProps,
+  {
+    /* Actions */
+  }
+)(App);
