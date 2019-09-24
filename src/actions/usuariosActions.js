af5db617e54ce9f0a5eb8e261af658c34f90b9ba
edevars <e.devars.dev@gmail.com>
2019-09-24 00:02:40 -0500

    :fire: Adding try catch

--- src/actions/usuariosActions.js
+++ src/actions/usuariosActions.js
@@ -1,11 +1,14 @@
-import {TRAER_TODOS} from '../types/usuariosTypes'
+import { TRAER_TODOS } from "../types/usuariosTypes";
 
 export const getAll = () => async dispatch => {
-  const response = await fetch(`https://jsonplaceholder.typicode.com/users`);
-  let users = await response.json();
-
-  dispatch({
-    type: TRAER_TODOS,
-    payload: users
-  });
+  try {
+    const response = await fetch(`https://jsonplaceholder.typicode.com/users`);
+    let users = await response.json();
+    dispatch({
+      type: TRAER_TODOS,
+      payload: users
+    });
+  } catch (error) {
+    console.error(error.message);
+  }
 };
