dc6f59f90d12dd796baf19a9432b80714a0ee624
edevars <e.devars.dev@gmail.com>
2019-09-23 21:21:53 -0500

    :sparkles: Creating store and using redux

--- src/Components/App.js
+++ src/Components/App.js
@@ -5,6 +5,13 @@ import Users from "./Users/Users";
 import Layout from "./Layout";
 import Tasks from "./tasks";
 
+import {createStore} from 'redux'
+import { Provider } from 'react-redux'
+
+const store = createStore(
+  {},//Todos los reducers
+  {}//Estado inicial
+)
 
 const App = () => (
   <BrowserRouter>
