ef613b28cc8eb3824ec7dc0ef92b9abd39b393d6
edevars <e.devars.dev@gmail.com>
2019-09-25 11:56:25 -0500

    :lipstick: Adding a default margin to the layout

--- src/Components/Layout.js
+++ src/Components/Layout.js
@@ -1,19 +1,30 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React from "react";
+import { Link } from "react-router-dom";
 
-const Layout = (props) => (
-    <>
-        <nav className="navbar is-dark" role="navigation" aria-label="main navigation">
-            <div className="navbar-brand">
-                <a className="navbar-item" href="https://bulma.io">
-                    <img alt="Logo" src="https://lh3.googleusercontent.com/Ax2wQYxjDITuZEpc6K9EDYPG7C839tb4PApia4Tmf18u8XehB-twqhVgDVPgxxExkr4" />
-                </a>
-                <Link className="navbar-link is-primary" to="/">Usuarios</Link>
-                <Link className="navbar-link is-primary" to="/tareas">Tareas</Link>
-            </div>
-        </nav>
-        {props.children}
-    </>
-)
+const Layout = props => (
+  <>
+    <nav
+      className="navbar is-dark"
+      role="navigation"
+      aria-label="main navigation"
+    >
+      <div className="navbar-brand">
+        <a className="navbar-item" href="https://bulma.io">
+          <img
+            alt="Logo"
+            src="https://lh3.googleusercontent.com/Ax2wQYxjDITuZEpc6K9EDYPG7C839tb4PApia4Tmf18u8XehB-twqhVgDVPgxxExkr4"
+          />
+        </a>
+        <Link className="navbar-link is-primary" to="/">
+          Usuarios
+        </Link>
+        <Link className="navbar-link is-primary" to="/tareas">
+          Tareas
+        </Link>
+      </div>
+    </nav>
+    <div className="container section">{props.children}</div>
+  </>
+);
 
-export default Layout
+export default Layout;
