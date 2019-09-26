e8ccef79489368ee8d6c389fbeda02af1e8ce014
edevars <e.devars.dev@gmail.com>
2019-09-25 20:28:47 -0500

    :zap: Loading and error handling in posts

--- src/Components/posts/index.js
+++ src/Components/posts/index.js
@@ -2,6 +2,8 @@ import React, { Component } from "react";
 import { connect } from "react-redux";
 import * as usuariosActions from "../../actions/usuariosActions";
 import * as publicacionesActions from "../../actions/publicacionesActions";
+import Spinner from "../general/Spinner";
+import Fatal from "../general/Fatal";
 
 class Posts extends Component {
   async componentDidMount() {
@@ -14,7 +16,36 @@ class Posts extends Component {
   render() {
     const { index } = this.props.match.params;
     console.log(this.props);
-    return <h1 className="title">Publicacion del usuario {index}</h1>;
+
+    const {
+      loading: loadingPosts,
+      error: errorPosts
+    } = this.props.publicacionesReducer;
+
+    const {
+      loading: loadingUsers,
+      error: errorUsers
+    } = this.props.usuariosReducer;
+
+    const showContent = () => {
+      if (loadingUsers || loadingPosts) {
+        return <Spinner></Spinner>;
+      } else {
+        return <h1 className="title">Publicacion del usuario {index}</h1>;
+      }
+    };
+
+    const handleErrorWithContent = () => {
+      if (errorUsers) {
+        return <Fatal message={errorUsers}></Fatal>;
+      } else if (errorPosts) {
+        return <Fatal message={errorPosts}></Fatal>;
+      } else {
+        return showContent();
+      }
+    };
+
+    return <>{handleErrorWithContent()}</>;
   }
 }
 
